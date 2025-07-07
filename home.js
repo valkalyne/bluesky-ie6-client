4b83dac8f65a79946541bd8ba32813fa71ba977d
anka <133736967+valkalyne@users.noreply.github.com>
2025-07-08 06:07:59 +1200

    BETTER cookies.

--- home.js
+++ home.js
@@ -141,7 +141,7 @@ function setCookie(cname, cvalue, exdays) {
     var d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/"; + ";Secure" + ";SameSite=Strict"
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;Secure;SameSite=Strict"
 }
 
 function getCookie(cname) {
