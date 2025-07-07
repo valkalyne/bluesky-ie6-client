1769c7aba3a0c46142e88d6f119567696a78ffca
anka <133736967+valkalyne@users.noreply.github.com>
2025-07-08 09:47:37 +1200

    Update home.js

--- home.js
+++ home.js
@@ -19,23 +19,23 @@ function work() {
 }
 
 function login() {
-    var XmlHttp
+    var xhr
     if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
-        XmlHttp = new XMLHttpRequest()
+        xhr = new XMLHttpRequest()
     }
     else {
         // code for IE6, IE5
-        XmlHttp = new ActiveXObject("Microsoft.XMLHTTP")
+        xhr = new ActiveXObject("Microsoft.XMLHTTP")
     }
-    XmlHttp.open("POST", "https://bsky.social/xrpc/com.atproto.server.createSession", false)
-    XmlHttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
-    XmlHttp.send(JSON.stringify({
+    xhr.open("POST", "https://bsky.social/xrpc/com.atproto.server.createSession", false)
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
+    xhr.send(JSON.stringify({
         "identifier": document.getElementById("usernameinput").value,
         "password": document.getElementById("passwordinput").value
     }));
     try{
-        auth = JSON.parse(XmlHttp.responseText)
+        auth = JSON.parse(xhr.responseText)
     }
     catch(error){
         alert("something has gone Horribly Wrong: " + error.message)
@@ -52,43 +52,43 @@ function login() {
         location.reload()
     }
     else {
-        alert(JSON.parse(XmlHttp.responseText).message)
+        alert(JSON.parse(xhr.responseText).message)
     }
 }
 
 function refreshsession() {
-    var XmlHttp
+    var xhr
     if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
-        XmlHttp = new XMLHttpRequest()
+        xhr = new XMLHttpRequest()
     }
     else {
         // code for IE6, IE5
-        XmlHttp = new ActiveXObject("Microsoft.XMLHTTP")
+        xhr = new ActiveXObject("Microsoft.XMLHTTP")
     }
-    XmlHttp.open("POST", "https://bsky.social/xrpc/com.atproto.server.refreshSession", false)
-    XmlHttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
-    XmlHttp.setRequestHeader("authorization", "Bearer " + auth.refreshJwt)
-    XmlHttp.send(null);
+    xhr.open("POST", "https://bsky.social/xrpc/com.atproto.server.refreshSession", false)
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
+    xhr.setRequestHeader("authorization", "Bearer " + auth.refreshJwt)
+    xhr.send(null);
     try{
-        auth = JSON.parse(XmlHttp.responseText)
+        auth = JSON.parse(xhr.responseText)
     }
     catch(error){
-        var status = XmlHttp.status
+        var status = xhr.status
         if (status = 403){
             alert("session expired. please log in again")
             auth = 
             logout()
         } 
         else{
-            alert(XmlHttp.status)
+            alert(xhr.status)
         }
     }
     if (auth.active) {
         $.jStorage.set("auth",JSON.stringify(auth))
     }
     else {
-        alert(XmlHttp.responseText)
+        alert(xhr.responseText)
     }
 }
 
@@ -98,58 +98,59 @@ function refresh() {
         document.getElementById("timelineheader").removeChild(document.getElementById("welcome"))
         document.getElementById("bar").style.display = "block"
         refreshsession()
-        var XmlHttp
+        var xhr
         if (window.XMLHttpRequest) {
             // code for IE7+, Firefox, Chrome, Opera, Safari
-            XmlHttp = new XMLHttpRequest()
+            xhr = new XMLHttpRequest()
         }
         else {
             // code for IE6, IE5
-            XmlHttp = new ActiveXObject("Microsoft.XMLHTTP")
+            xhr = new ActiveXObject("Microsoft.XMLHTTP")
         }
-        loaduserinfo(XmlHttp)
+        loaduserinfo(xhr)
     }
     else {
         document.getElementById("bar").removeChild(document.getElementById("signedin"))
         document.getElementById("bar").style.display = "block"
     }
+    document.getElementById("timeline").innerHTML = "Loading..."
     refreshtimeline()
 }
 
 function logout() {
-    var XmlHttp
+    var xhr
     if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
-        XmlHttp = new XMLHttpRequest()
+        xhr = new XMLHttpRequest()
     }
     else {
         // code for IE6, IE5
-        XmlHttp = new ActiveXObject("Microsoft.XMLHTTP")
+        xhr = new ActiveXObject("Microsoft.XMLHTTP")
     }
-    XmlHttp.open("POST", "https://bsky.social/xrpc/com.atproto.server.deleteSession", true)
-    XmlHttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
-    XmlHttp.setRequestHeader("authorization", "Bearer " + auth.refreshJwt)
-    XmlHttp.send(null);
+    xhr.open("POST", "https://bsky.social/xrpc/com.atproto.server.deleteSession", true)
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
+    xhr.setRequestHeader("authorization", "Bearer " + auth.refreshJwt)
+    xhr.send(null);
     $.jStorage.deleteKey("auth")
     location.reload()
 }
 
 
 function loaduserinfo() {
-    var XmlHttp
+    var xhr
     if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
-        XmlHttp = new XMLHttpRequest()
+        xhr = new XMLHttpRequest()
     }
     else {
         // code for IE6, IE5
-        XmlHttp = new ActiveXObject("Microsoft.XMLHTTP")
+        xhr = new ActiveXObject("Microsoft.XMLHTTP")
     }
-    XmlHttp.open("GET", "https://bsky.social/xrpc/app.bsky.actor.getProfile?actor=" + auth.handle, false)
-    XmlHttp.setRequestHeader("authorization", "Bearer " + auth.accessJwt)
-    XmlHttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
-    XmlHttp.send(null);
-    var myprofile = JSON.parse(XmlHttp.responseText)
+    xhr.open("GET", "https://bsky.social/xrpc/app.bsky.actor.getProfile?actor=" + auth.handle, false)
+    xhr.setRequestHeader("authorization", "Bearer " + auth.accessJwt)
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
+    xhr.send(null);
+    var myprofile = JSON.parse(xhr.responseText)
     document.getElementById("profilepaneusername").innerText = myprofile.handle
     document.getElementById("mypfp").src = myprofile.avatar
     document.getElementById("profilepanedisplayname").innerText = myprofile.displayName
@@ -161,14 +162,14 @@ function loaduserinfo() {
 
 function post(){
     refreshsession()
-    var XmlHttp
+    var xhr
     if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
-        XmlHttp = new XMLHttpRequest()
+        xhr = new XMLHttpRequest()
     }
     else {
         // code for IE6, IE5
-        XmlHttp = new ActiveXObject("Microsoft.XMLHTTP")
+        xhr = new ActiveXObject("Microsoft.XMLHTTP")
     }
 
     var post = {
@@ -185,102 +186,109 @@ function post(){
         alert("Write something!")
     }
     else{
-        XmlHttp.open("POST", "https://bsky.social/xrpc/com.atproto.repo.createRecord", false)
-        XmlHttp.setRequestHeader("authorization", "Bearer " + auth.accessJwt)
-        XmlHttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
-        XmlHttp.send(JSON.stringify(post));
-        if (JSON.parse(XmlHttp.responseText).error){
-            alert(JSON.parse(XmlHttp.responseText).message)
+        xhr.open("POST", "https://bsky.social/xrpc/com.atproto.repo.createRecord", false)
+        xhr.setRequestHeader("authorization", "Bearer " + auth.accessJwt)
+        xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
+        xhr.send(JSON.stringify(post));
+        if (JSON.parse(xhr.responseText).error){
+            alert(JSON.parse(xhr.responseText).message)
         }
         else{
             alert("post sent!")
+            document.getElementById("timeline").innerHTML = "Loading..."
             refreshtimeline()
         }
     }
 }
 
 function refreshtimeline(){
-    document.getElementById("timeline").innerHTML = "Loading..."
-    refreshtimelinebuffer()
-}
-
-function refreshtimelinebuffer(){
     if (auth){
         refreshsession()
     }
-    var XmlHttp
+    var xhr
     if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
-        XmlHttp = new XMLHttpRequest()
+        xhr = new XMLHttpRequest()
     }
     else {
         // code for IE6, IE5
-        XmlHttp = new ActiveXObject("Microsoft.XMLHTTP")
+        xhr = new ActiveXObject("Microsoft.XMLHTTP")
     }
     if (auth) {
-        XmlHttp.open("GET", "https://bsky.social/xrpc/app.bsky.feed.getTimeline?limit="+feedlength+"&cursor="+getcurrentiso(), false)
-        XmlHttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
-        XmlHttp.setRequestHeader("authorization", "Bearer " + auth.accessJwt)
-        XmlHttp.setRequestHeader("cache-control", "no-cache, no-store, max-age=0")
-        XmlHttp.setRequestHeader("Expires", "Tue, 01 Jan 1980 1:00:00 GMT")
-        XmlHttp.setRequestHeader("Pragma", "no-cache")
+        xhr.open("GET", "https://bsky.social/xrpc/app.bsky.feed.getTimeline?limit="+feedlength+"&cursor="+getcurrentiso(), false)
+        xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
+        xhr.setRequestHeader("authorization", "Bearer " + auth.accessJwt)
+        xhr.setRequestHeader("cache-control", "no-cache, no-store, max-age=0")
+        xhr.setRequestHeader("Expires", "Tue, 01 Jan 1980 1:00:00 GMT")
+        xhr.setRequestHeader("Pragma", "no-cache")
     }
     else {
-        XmlHttp.open("GET", "https://public.api.bsky.app/xrpc/app.bsky.feed.getFeed?feed=at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.generator/whats-hot&limit="+feedlength+"&lang=en", false)
-        //XmlHttp.open("GET", "stupid.json", false)
-        XmlHttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
+        xhr.open("GET", "https://public.api.bsky.app/xrpc/app.bsky.feed.getFeed?feed=at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.generator/whats-hot&limit="+feedlength+"&lang=en", false)
+        //xhr.open("GET", "stupid.json", false)
+        xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
     }
-    XmlHttp.send(null);
-    feed = XmlHttp.responseText
-    parsedfeed = JSON.parse(feed)
-    cursor = parsedfeed.cursor
-    postcount = 0
-    document.getElementById("timeline").innerHTML = ""
-    for (var i in parsedfeed.feed) {
-        htmlpost(parsedfeed.feed[i].post,parsedfeed.feed[i].reason,parsedfeed.feed[i].reply)
-        postcount += 1
+    
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            feed = xhr.responseText
+            parsedfeed = JSON.parse(feed)
+            cursor = parsedfeed.cursor
+            postcount = 0
+            document.getElementById("timeline").innerHTML = ""
+            for (var i in parsedfeed.feed) {
+                postcount += 1
+                htmlpost(parsedfeed.feed[i].post,parsedfeed.feed[i].reason,parsedfeed.feed[i].reply)
+            }
+        }
     }
+    xhr.send(null);
 }
+
+
 
 function ShowMore(){
     if(cursor){
         if (auth){
             refreshsession()
         }
-        var XmlHttp
+        var xhr
         if (window.XMLHttpRequest) {
             // code for IE7+, Firefox, Chrome, Opera, Safari
-            XmlHttp = new XMLHttpRequest()
+            xhr = new XMLHttpRequest()
         }
         else {
             // code for IE6, IE5
-            XmlHttp = new ActiveXObject("Microsoft.XMLHTTP")
+            xhr = new ActiveXObject("Microsoft.XMLHTTP")
         }
         if (auth) {
-            XmlHttp.open("GET", "https://bsky.social/xrpc/app.bsky.feed.getTimeline?limit="+feedlength+"&cursor="+cursor, false)
-            XmlHttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
-            XmlHttp.setRequestHeader("authorization", "Bearer " + auth.accessJwt)
-            XmlHttp.setRequestHeader("cache-control", "no-cache, no-store, max-age=0")
-            XmlHttp.setRequestHeader("Expires", "Tue, 01 Jan 1980 1:00:00 GMT")
-            XmlHttp.setRequestHeader("Pragma", "no-cache")
+            xhr.open("GET", "https://bsky.social/xrpc/app.bsky.feed.getTimeline?limit="+feedlength+"&cursor="+cursor, false)
+            xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
+            xhr.setRequestHeader("authorization", "Bearer " + auth.accessJwt)
+            xhr.setRequestHeader("cache-control", "no-cache, no-store, max-age=0")
+            xhr.setRequestHeader("Expires", "Tue, 01 Jan 1980 1:00:00 GMT")
+            xhr.setRequestHeader("Pragma", "no-cache")
         }
         else {
-            XmlHttp.open("GET", "https://public.api.bsky.app/xrpc/app.bsky.feed.getFeed?feed=at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.generator/whats-hot&limit="+feedlength+"&lang=en&cursor="+cursor, false)
-            //XmlHttp.open("GET", "stupid.json", false)
-            XmlHttp.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
+            xhr.open("GET", "https://public.api.bsky.app/xrpc/app.bsky.feed.getFeed?feed=at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.generator/whats-hot&limit="+feedlength+"&lang=en&cursor="+cursor, false)
+            //xhr.open("GET", "stupid.json", false)
+            xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8")
         }
-        XmlHttp.send(null);
-        feed = XmlHttp.responseText
-        parsedfeed = JSON.parse(feed)
-        cursor = parsedfeed.cursor
-        for (var i in parsedfeed.feed) {
-            htmlpost(parsedfeed.feed[i].post,parsedfeed.feed[i].reason,parsedfeed.feed[i].reply)
-            postcount += 1
-        }
+        xhr.send(null);
+        //feed = xhr.responseText
+        //parsedfeed = JSON.parse(feed)
+        //cursor = parsedfeed.cursor
+        //for (var i in parsedfeed.feed) {
+        //    htmlpost(parsedfeed.feed[i].post,parsedfeed.feed[i].reason,parsedfeed.feed[i].reply)
+        //    postcount += 1
+        //}
     }
     else {
         alert("japed. Sorry")
     }
+}
+
+function bink(){
+    document.getElementById("timeline").innerHTML = "Loading..."
 }
 
 
